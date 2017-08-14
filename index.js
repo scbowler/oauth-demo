17a8baded9a60b75ec092d2bab1a81a5cb4573c8
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-08-14 13:59:54 -0700

    Updates were made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({words: 'there are some words here'});
 });
 
 const PORT = process.env.PORT || 5000;
