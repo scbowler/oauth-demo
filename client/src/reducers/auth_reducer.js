566825bdae8702b08a671f5b0982b27ae5267491
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-19 10:43:18 -0800

    Working oauth example

---
+++ client/src/reducers/auth_reducer.js
@@ -0,0 +1,10 @@
+import types from '../actions/types';
+
+export default function(state = null, action){
+    switch(action.type){
+        case types.FETCH_USER:
+            return action.payload || false;
+        default:
+            return state;
+    }
+}
